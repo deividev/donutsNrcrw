1e0e51b4ec1e804bcc07e609cb90bd66933436c7
david <deivid.develop@gmail>
2020-10-02 17:13:00 +0200

    Added card contact and icons

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  faInstagram, faInstagramSquare, faBehanceSquare,} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope as faEnvelope,} from '@fortawesome/free-regular-svg-icons';
+import { faPhoneAlt as faPhoneAlt,} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  faMobile = faPhoneAlt;
+  faEmail = faEnvelope;
+  faInstagram = faInstagramSquare;
+  faBehance = faBehanceSquare
 
   constructor() { }
 
